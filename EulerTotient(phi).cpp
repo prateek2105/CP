17a272458787accb2141439a2163a87bdd39059c
Ragnar <138352581+prateek2105@users.noreply.github.com>
2026-01-20 18:19:14 +0530

    Create EulerTotient(phi).cpp

---
+++ EulerTotient(phi).cpp
@@ -0,0 +1,14 @@
+vector<int> phi;
+
+void phi_1_to_n(int n) {
+    phi.assign(n + 1, 0);
+    for (int i = 0; i <= n; i++)
+        phi[i] = i;
+
+    for (int i = 2; i <= n; i++) {
+        if (phi[i] == i) {
+            for (int j = i; j <= n; j += i)
+                phi[j] -= phi[j] / i;
+        }
+    }
+}
